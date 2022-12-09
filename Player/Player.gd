09315bb90637a26f4e0eae29462c204d4dec9cd5
Maijs Garais <maireadsbooks@gmail.com>
2022-12-09 15:05:36 +0000

    add idle animation blend space

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
 onready var animTree: = $AnimationTree
+onready var animState = animTree.get("parameters/playback")
 
 const ACCELERATION: = 500
-const MAX_SPEED: = 60
+const MAX_SPEED: = 40
 const FRICTION: = 500
 
 var velocity: = Vector2.ZERO
+
+func _ready() -> void:
+	animTree.active = true;
 
 func _physics_process(delta: float) -> void:
 	var input_vector: = Vector2.ZERO
@@ -19,8 +23,11 @@ func _physics_process(delta: float) -> void:
 		# accelerate
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		animTree.set("parameters/Walk/blend_position", input_vector)
+		animTree.set("parameters/Idle1D/blend_position", input_vector.x)
+		animState.travel("Walk")
 	else:
 		# decelerate
+		animState.travel("Idle1D")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move()
