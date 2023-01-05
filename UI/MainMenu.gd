a46edacb3a846842e99154de8859267b2412b2d8
Maijs Garais <maireadsbooks@gmail.com>
2023-01-05 15:43:42 +0000

    add simple main menu

---
+++ UI/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready() -> void:
+	$VBoxContainer/StartBtn.grab_focus()
+
+func _on_StartBtn_pressed() -> void:
+	get_tree().change_scene("res://World.tscn")
+
+func _on_CreditsBtn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_QuitBtn_pressed() -> void:
+	get_tree().quit()
